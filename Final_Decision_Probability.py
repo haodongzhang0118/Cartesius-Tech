68d6a47ead80df06c91cafe07764f64e20ca7f28
guilherme <guilherme.dornelles@edu.pucrs.br>
2023-05-18 23:46:17 -0300

    Fixing endpoints

--- Final_Decision_Probability.py
+++ Final_Decision_Probability.py
@@ -1,17 +1,22 @@
 import tensorflow as tf
 import numpy as np
 
+
 def create_model():
     model = tf.keras.Sequential([
         tf.keras.layers.Dense(1, activation="sigmoid", input_shape=(9, ))
     ])
-    model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='sgd', loss='binary_crossentropy',
+                  metrics=['accuracy'])
     return model
+
 
 model = create_model()
 model.load_weights("logistic_weights.h5")
 
+
 def make_prediction(GPA, SAT_english, SAT_math, SAT_essay, activity, personal_statement, residency, race, gender):
-    features = np.array([[GPA, SAT_english, SAT_math, SAT_essay, activity, personal_statement, residency, race, gender]])
+    features = np.array([[GPA, SAT_english, SAT_math, SAT_essay,
+                        activity, personal_statement, residency, race, gender]])
     result = model.predict(features)[0, 0]
     return "accepted" if result == 1 else "rejected"
